7c939783607f698fee5824ebc034ecbb94e7eb76
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-08-31 17:04:42 +0400

    delete file

---
+++ products-service/functions/getProductsById/getProductsById.js
@@ -0,0 +1,20 @@
+import products from "../productList.json";
+
+const response = (products = {}, status = 200) => ({
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Origin": "*",
+  },
+  statusCode: status,
+  body: JSON.stringify(products),
+});
+
+export const handler = async (event) => {
+  const { productId } = event.pathParameters || {};
+  const product = await products.find(({ id }) => id === productId);
+  if (!product) {
+    return response({ message: "Error: Product not found!" }, 400);
+  }
+  return response({ ...product }, 200);
+};
