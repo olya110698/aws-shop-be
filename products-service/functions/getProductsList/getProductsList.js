bdb0d5eff68eb086473537c7d36a08cbb7e212bd
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-09-02 15:39:39 +0400

    changed files

--- products-service/functions/getProductsList/getProductsList.js
+++ products-service/functions/getProductsList/getProductsList.js
@@ -1,13 +1,14 @@
-import products from "../productList.json";
+
+import products from '../productList.json';
 
 const handleResponse = (products = {}, status = 200) => ({
   headers: {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Origin": "*",
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Methods': '*',
+    'Access-Control-Allow-Origin': '*',
   },
   statusCode: status,
   body: JSON.stringify(products),
 });
 
-export const handler = async (event) => await handleResponse(products);
+export const handler = async event => await handleResponse(products);
