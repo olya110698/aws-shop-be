296f509ba7ae6852f82db178e8f14a747c54c187
olya110698 <78224530+olya110698@users.noreply.github.com>
2021-09-08 22:32:45 +0400

    feat: change file getProductsList.js

--- products-service/functions/getProductsList/getProductsList.js
+++ products-service/functions/getProductsList/getProductsList.js
@@ -1,14 +1,51 @@
-
-import products from '../productList.json';
+import { Client } from "pg";
+//const { Client } = require("pg");
 
 const handleResponse = (products = {}, status = 200) => ({
   headers: {
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Origin': '*',
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Origin": "*",
   },
   statusCode: status,
   body: JSON.stringify(products),
 });
 
-export const handler = async event => await handleResponse(products);
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+
+const credentials = {
+  user: PG_USERNAME,
+  host: PG_HOST,
+  database: PG_DATABASE,
+  password: PG_PASSWORD,
+  port: PG_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
+
+let data_export = {},
+  error_code = 200;
+
+export const handler = async (event) => {
+  const client = new Client(credentials);
+
+  await client.connect();
+
+  await client
+    .query(
+      "SELECT products.*, stocks.count FROM products LEFT JOIN stocks ON products.id = stocks.product_id"
+    )
+    .then((res) => {
+      data_export = res.rows;
+    })
+    .catch((e) => {
+      data_export = "DB Error 500:" + e.stack;
+      error_code = 500;
+    });
+
+  await client.end();
+
+  return await handleResponse({ data_export, error_code });
+};
